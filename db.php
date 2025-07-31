1024f14144a2f52645b6d2231bb726a48afe559b
jackonshiundu <jackonshiundu2019@gmail.com>
2025-07-31 17:27:09 +0300

    php mailer

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// Database configuration
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'john_doe';
+
+        $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+
+    ?>
